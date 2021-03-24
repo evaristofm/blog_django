3de82795e601011e2185f7312312e832056b91a4
evaristofm <evaristofm@gmail.com>
2021-03-24 01:24:03 -0300

    Modificando templates e debugar o profile

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
 
-    author = models.ForeignKey(User, related_name='posts',on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
